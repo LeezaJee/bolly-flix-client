d4db64bfde32bb0b55693b9060dc976c30c64f50
Mona Lisa <lisa.nadeem@outlook.de>
2022-06-02 10:59:56 +0200

    imported axios and deleted movies in the this.state movies array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,47 +8,7 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: "VEER ZAARA",
-          Description:
-            "The Indian pilot Veer Pratap Singh saves the life of the young Pakistani woman Zaara. They spend one day together, which brings them closer and allows them to experience their common roots. 22 years later, the young lawyer Saamiya Siddiqui is supposed to uncover the mystery surrounding Veer, who has been living neglected in a Pakistani prison for a good two decades.",
-          ImagePath: (
-            //this is the code you gave me to display an image but it didn't work for neither of the three movies
-            <img
-              src="https://img.goldposter.com/2020/03/veer-zaara_poster_goldposter_com_1.jpg"
-              crossOrigin="anonymous"
-            />
-          ),
-        },
-        {
-          _id: 2,
-          Title: "Good Newwz",
-          Description:
-            "After several failed attempts at conceiving a baby naturally, two couples with the same surname consult Mumbai's best fertility doctors and opt for IVF. As they await the good news, the doctor shares a rather negligent sperm mix-up, leaving both the couples in a state of agony and helplessness.",
-          ImagePath: (
-            //this <img> tag is not showing an image either
-            <img
-              src="https://img.goldposter.com/2019/12/good-newwz_poster_goldposter_com_9.jpg"
-              crossOrigin="anonymous"
-            />
-          ),
-        },
-        {
-          _id: 3,
-          Title: "MOM",
-          Description:
-            "Devaki is a biology teacher at the same school as her stepdaughter Arya. Although, Devaki is a caring mother, Arya remains distant. One unfortunate night, Arya is brutally attacked raped at a party and when the justice system failed them, Devaki takes matters in her own hands to find justice for her daughter.",
-          ImagePath: (
-            //this <img> tag is not showing an image either
-            <img
-              src="https://media-cache.cinematerial.com/p/500x/0kzdgql5/mom-indian-movie-poster.jpg?v=1555579912"
-              crossOrigin="anonymous"
-            />
-          ),
-        },
-      ],
+      movies: [],
       selectedMovie: null,
     };
   }
