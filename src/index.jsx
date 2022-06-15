50144be0cfda8699238a066385b3fc1c2a38dacd
Mona Lisa <lisa.nadeem@outlook.de>
2022-06-15 11:05:29 +0200

    wrapped application in a <Container> in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import Container from "react-bootstrap/Container";
 
 //import default MovieView from "./components/main-view/main-view";
 //you can only export one item using the default keyword (without curly braces)
@@ -13,7 +14,11 @@ import "./index.scss";
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView />; //as opposed to <MainView></MainView> (if there are no nested elements inside)
+    return (
+      <Container>
+        <MainView />
+      </Container>
+    );
   }
 }
 
