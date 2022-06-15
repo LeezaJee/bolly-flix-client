0db92a8549898f56008bee69c43b28f23d221c4d
Mona Lisa <lisa.nadeem@outlook.de>
2022-06-15 11:02:42 +0200

    replaced jsx elements with form components

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
@@ -34,5 +36,27 @@ export function LoginView(props) {
         Submit
       </button>
     </form>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+>>>>>>> Stashed changes
   );
 }
