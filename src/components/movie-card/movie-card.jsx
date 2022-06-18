df1e5fe299439b2c65a3e8181868aff3cecf5a44
Mona Lisa <lisa.nadeem@outlook.de>
2022-06-18 13:15:04 +0200

    added row and col to card component and added prop.Types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props; //movieData = prop name extracted from main-view.jsx
+
     return (
-      <Card>
-        <Card.Img variant="top" src={movie.ImagePath} />
-        <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">
-            Open
-          </Button>
-        </Card.Body>
-      </Card>
+      <Row>
+        <Col>
+          <Card border="info">
+            <Card.Img variant="top" src={movie.ImagePath} />
+            <Card.Body>
+              <Card.Title>{movie.Title}</Card.Title>
+              <Card.Text>{movie.Description}</Card.Text>
+              <Button onClick={() => onMovieClick(movie)} variant="info">
+                Open
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string,
+    }),
+    Actors: PropTypes.arrayOf(PropTypes.string),
+    ImagePath: PropTypes.string,
+    Featured: PropTypes.bool,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
