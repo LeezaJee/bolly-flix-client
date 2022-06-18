64796514eb11a668d224e0d1d84c3ca436b28083
Mona Lisa <lisa.nadeem@outlook.de>
2022-06-18 13:12:16 +0200

    added registration button and styled login view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Form, Button, Card, Row, Col, Container } from "react-bootstrap";
 import { RegistrationView } from "../registration-view/registration-view";
+import axios from "axios";
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  //indicates whether to show RegistraionView or not - by default false
+  const [showRegister, setShowRegister] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,46 +18,50 @@ export function LoginView(props) {
     props.onLoggedIn(username);
   };
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShowRegister(!showRegister);
     return <RegistrationView />;
   };
 
   return (
     <Container>
       <Row>
-        <Card>
+        <Card border="info">
           <Card.Body>
             <Card.Title>Login</Card.Title>
-            <Form>
-              <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter Username"
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </Form.Group>
+            {showRegister === false ? (
+              <Form>
+                <Form.Group controlId="formUsername">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your Username"
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter Password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
-            </Form>
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter your Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
 
-            <Button variant="info" type="submit" onClick={handleSubmit}>
-              Submit
-            </Button>
-            <Button
-              variant="outline-secondary"
-              type="button"
-              onClick={handleClick}
-            >
-              Register here
-            </Button>
+                <div className="mt-2">
+                  <Button variant="info" type="submit" onClick={handleSubmit}>
+                    Submit
+                  </Button>
+                  <Button variant="link" onClick={handleClick}>
+                    Register
+                  </Button>
+                </div>
+              </Form>
+            ) : (
+              <RegistrationView toggleLogin={handleClick} />
+            )}
           </Card.Body>
         </Card>
       </Row>
