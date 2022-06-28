ee312b62d615348067512088d3233b927ea64876
Mona Lisa <lisa.nadeem@outlook.de>
2022-06-28 15:07:57 +0200

    added form verification for registration-view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,35 +1,80 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { Form, Button } from "react-bootstrap";
-
-import "./registration-view.scss";
 import axios from "axios";
+import PropTypes from "prop-types";
+import { Row, Col, Form, Button, Container } from "react-bootstrap";
+import "./registration-view.scss";
 
 export function RegistrationView(props) {
+  const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
 
+  const [nameErr, setNameErr] = useState("");
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  const [emailErr, setEmailErr] = useState("");
+
   const { toggleLogin } = props;
+
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!name) {
+      setNameErr("Name Required");
+      isReq = false;
+    } else if (name.length < 5) {
+      setNameErr("Name is too short");
+      isReq = false;
+    }
+    if (!username) {
+      setUsernameErr("Username Required");
+      isReq = false;
+    } else if (username.length < 5) {
+      setUsernameErr("Username must be at least 5 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password Required");
+      isReq = false;
+    } else if (password.length < 6) {
+      setPasswordErr("Password must be at least 6 characters long");
+      isReq = false;
+    }
+    if (!email) {
+      setEmailErr("Email Required");
+      isReq = false;
+    } else if (email.indexOf("@") === -1) {
+      setEmailErr("Invalid Email");
+      isReq = false;
+    }
+
+    return isReq;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios
-      .post("https://bolly-flix.herokuapp.com/users", {
-        Username: username,
-        Password: password,
-        Email: email,
-      })
-      .then((response) => {
-        const data = response.data;
-        console.log(data);
-        window.open("/", "_self");
-        // the second argument '_self' is necessary so that the page will open in the current tab
-      })
-      .catch((e) => {
-        console.log("error registering the user");
-        alert("Something was not entered right");
-      });
+    const isReq = validate();
+    if (isReq) {
+      axios
+        .post("https://bolly-flix.herokuapp.com/users", {
+          Name: name,
+          Username: username,
+          Password: password,
+          Email: email,
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          alert("Registration successful, please login");
+          window.open("/", "_self");
+          // the second argument '_self' is necessary so that the page will open in the current tab
+        })
+        .catch((response) => {
+          console.error(response);
+          alert("unable to register");
+        });
+    }
   };
 
   return (
