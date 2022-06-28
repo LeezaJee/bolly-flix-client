911a94578219f26497cd9b879733b1d117f8a547
Mona Lisa <lisa.nadeem@outlook.de>
2022-06-28 15:06:52 +0200

    added form validation for login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,32 +3,55 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import { Form, Button, Card, Row, Col, Container } from "react-bootstrap";
 import { RegistrationView } from "../registration-view/registration-view";
-
 import "./login-view.scss";
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  // Declare hook for each input
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
   //indicates whether to show RegistraionView or not - by default false
   const [showRegister, setShowRegister] = useState(false);
 
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username Required");
+      isReq = false;
+    } else if (username.length < 5) {
+      setUsernameErr("Username must be 5 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password Required");
+      isReq = false;
+    } else if (password.length < 6) {
+      setPassword("Password must be 6 characters long");
+      isReq = false;
+    }
+    return isReq;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    /* Send a request to the server for authentication */
-    axios
-      .post("https://bolly-flix.herokuapp.com/login", {
-        Username: username,
-        Password: password,
-      })
-      .then((response) => {
-        const data = response.data;
-        props.onLoggedIn(data);
-        //when user logs in props.onLoggedIn(data) is passed to LoginView + triggers onLoggedIn(authData) in MainView
-        //this updates state with the logged in authData
-      })
-      .catch((e) => {
-        console.log("There is no such user");
-      });
+    const isReq = validate();
+    if (isReq) {
+      /* Send request to the server for authentication */
+      axios
+        .post("https://bolly-flix.herokuapp.com/login", {
+          Username: username,
+          Password: password,
+        })
+        .then((response) => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch((e) => {
+          console.log("no such user");
+        });
+    }
   };
 
   const handleClick = (e) => {
