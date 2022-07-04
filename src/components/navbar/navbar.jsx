1447d829587f01808bb6edfae18e5be19bc61197
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-04 15:23:30 +0200

    added button to logout in Navbar and link to register

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+
+export function Navbar({ user }) {
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open("/", "_self");
+  };
+
+  const isAuth = () => {
+    if (typeof window == "undefined") {
+      return false;
+    }
+    if (localStorage.getItem("token")) {
+      return localStorage.getItem("token");
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <Navbar
+      className="main-nav"
+      sticky="top"
+      bg="dark"
+      expand="lg"
+      variant="dark"
+    >
+      <Container fluid>
+        <Navbar.Brand className="navbar-logo" href="/">
+          BollyFlix Movies
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            {isAuth() && (
+              <Button
+                variant="link"
+                onClick={() => {
+                  this.onLoggedOut();
+                }}
+              >
+                Logout
+              </Button>
+            )}
+            {!isAuth() && <Nav.Link href="/register">Sign-Up</Nav.Link>}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
