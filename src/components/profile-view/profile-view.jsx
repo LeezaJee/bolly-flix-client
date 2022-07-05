00260090e0254fca8cfb81c36528b1248029ccbf
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-05 13:38:03 +0200

    adjusted profile-view code and replaced HTML code with  Container, Row and Col's

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,66 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { Form, Button, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import "./profile-view.scss";
-import axios from "axios";
-import UserInfo from "./user-info";
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+import { Container, Button, Col, Row, Card } from 'react-bootstrap'
+import { UserInfo } from './user-info'
+import FavoriteMovies from './favorite-movies'
+import { UpdateUser } from './update-user'
+import './profile-view.scss'
 
-export function ProfileView ({ movies, onUpdatedUserInfo}) {
-    
-    const [ user, setUser ] = useState({ })
+export function ProfileView({ movies }) {
+    const [user, setUser] = useState({
+        Username: '',
+        Email: '',
+        FavoriteMovies: [],
+    })
 
-    const favoriteMovieList = movies.filter({movies} => { });
-
-    const getUser = () => {}
-
-    const handleSubmit = (e) => {}
-
-    const removeFav = (id) => {}
-
-    const handleUpdate = (e) => {};
+    const favoriteMovieList = movies.filter((movies) => {
+        return user.FavoriteMovies.includes(movies._id)
+    })
 
     useEffect(() => {
+        let isMounted = true
+        isMounted && getUser()
+        return () => {
+            isMounted = false
+        }
     }, [])
 
-return (
-    <div>
-        <UserInfo name={user.Username} email={user.Email}/>
-    <div>
-    <h2>Favorite Movies</h2>
-    {favoriteMovieList.map((movies) => {
-        return (
-            <div key={movies._id}>
-            <img src={movies.ImagePath} />
-            <Link to={`/movies/${movies._id}`}>
-            <h4>{movies.Title}</h4>
-            </Link>
-            <button variant="secondary" onClick={() => removeFav(movies._id)}>Remove from List</button>
-            </div>
-        )
-    })
-}
-</div>
+    return (
+        <Container>
+            <Row>
+                <Col xs={12} sm={4}>
+                    <Card>
+                        <Card.Body>
+                            <UserInfo name={user.Username} email={user.Email} />
+                        </Card.Body>
+                    </Card>
+                </Col>
 
-<form className="profile-form" onSubmit={(e) => handleSubmit(e)}>
-<h2>Want to change some info?</h2>
-<label>Username:</label>
-<input type="text" name="Username" defaultValue={user.Username} onChange={e => handleUpdate(e)} />
+                <Col xs={12} sm={8}>
+                    <Card>
+                        <Card.Body>
+                            <UpdateUser
+                                handleSubmit={handleSubmit}
+                                handleUpdate={handleUpdate}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
 
-<label>Password</label>
-<input type="password" name="password" defaultValue={user.Password} onChange={e => handleUpdate(e)} />
-
-<label>Email address</label>
-<input type="email" name="email" defaultValue={user.Email} onChange={e => handleUpdate(e)} />
-
-</form>
-
-
-
-
-
-
-
-
-
-
+            <FavoriteMovies favoriteMovieList={favoriteMovieList} />
+        </Container>
+    )
 }
