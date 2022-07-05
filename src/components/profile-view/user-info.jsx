0388a1ab7bcffaf2fea75824c9bf9c0cd5d4aa67
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-05 13:40:07 +0200

    adjusted user-info code and replaced HTML with Col, Row and Card

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React from 'react'
+import { Col, Row, Card } from 'react-bootstrap'
 
-function UserInfo({ email, name }) {
-  return (
-    <>
-      <p>User: {name}</p>
-      <p>Email: {email}</p>
-    </>
-  );
+export function UserInfo({ email, name }) {
+    return (
+        <>
+            <Col>
+                <Row>
+                    <Card className="bg-light" border="black">
+                        <Card.Title className="text-dark">Your Info</Card.Title>
+                        <Card.Body
+                            className="text-dark"
+                            style={{ textAlign: 'left' }}
+                        >
+                            <label
+                                htmlFor="username"
+                                className="userdata-label"
+                            >
+                                Username:
+                            </label>
+                            <Card.Text>{userdata.username}</Card.Text>
+                            <label htmlFor="email" className="userdata-label">
+                                Email:
+                            </label>
+                            <Card.Text>{userdata.email}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Col>
+        </>
+    )
 }
 
-export default UserInfo;
+export default UserInfo
