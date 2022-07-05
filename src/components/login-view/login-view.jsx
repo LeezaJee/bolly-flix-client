1b46829593ae9662facd8475c5db60e120f6a3c7
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-05 14:59:35 +0200

    replaced double quotes with simple quotes and changed width of login-card

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,121 +1,133 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
-import { Form, Button, Card, Row, Col, Container } from "react-bootstrap";
-import { RegistrationView } from "../registration-view/registration-view";
-import "./login-view.scss";
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+import { Form, Button, Card, Row, Col, Container } from 'react-bootstrap'
+import { RegistrationView } from '../registration-view/registration-view'
+import './login-view.scss'
 
 export function LoginView(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  // Declare hook for each input
-  const [usernameErr, setUsernameErr] = useState("");
-  const [passwordErr, setPasswordErr] = useState("");
-  //indicates whether to show RegistraionView or not - by default false
-  const [showRegister, setShowRegister] = useState(false);
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    // Declare hook for each input
+    const [usernameErr, setUsernameErr] = useState('')
+    const [passwordErr, setPasswordErr] = useState('')
+    //indicates whether to show RegistraionView or not - by default false
+    const [showRegister, setShowRegister] = useState(false)
 
-  // validate user inputs
-  const validate = () => {
-    let isReq = true;
-    if (!username) {
-      setUsernameErr("Username Required");
-      isReq = false;
-    } else if (username.length < 5) {
-      setUsernameErr("Username must be 5 characters long");
-      isReq = false;
+    // validate user inputs
+    const validate = () => {
+        let isReq = true
+        if (!username) {
+            setUsernameErr('Username Required')
+            isReq = false
+        } else if (username.length < 5) {
+            setUsernameErr('Username must be 5 characters long')
+            isReq = false
+        }
+        if (!password) {
+            setPasswordErr('Password Required')
+            isReq = false
+        } else if (password.length < 6) {
+            setPassword('Password must be 6 characters long')
+            isReq = false
+        }
+        return isReq
     }
-    if (!password) {
-      setPasswordErr("Password Required");
-      isReq = false;
-    } else if (password.length < 6) {
-      setPassword("Password must be 6 characters long");
-      isReq = false;
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const isReq = validate()
+        if (isReq) {
+            /* Send request to the server for authentication */
+            axios
+                .post('https://bolly-flix.herokuapp.com/login', {
+                    Username: username,
+                    Password: password,
+                })
+                .then((response) => {
+                    const data = response.data
+                    props.onLoggedIn(data)
+                })
+                .catch((e) => {
+                    console.log('no such user')
+                })
+        }
     }
-    return isReq;
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const isReq = validate();
-    if (isReq) {
-      /* Send request to the server for authentication */
-      axios
-        .post("https://bolly-flix.herokuapp.com/login", {
-          Username: username,
-          Password: password,
-        })
-        .then((response) => {
-          const data = response.data;
-          props.onLoggedIn(data);
-        })
-        .catch((e) => {
-          console.log("no such user");
-        });
+    const handleClick = (e) => {
+        e.preventDefault()
+        setShowRegister(!showRegister)
+        return <RegistrationView />
     }
-  };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    setShowRegister(!showRegister);
-    return <RegistrationView />;
-  };
+    return (
+        <Container className="loginContainer" fluid>
+            <Row className="justify-content-center login-view">
+                <Card id="login-card">
+                    <Card.Body>
+                        <Card.Title id="login-label">
+                            {<h3>Welcome to BollyFlix!</h3>}
+                        </Card.Title>
+                        {showRegister === false ? (
+                            <Form>
+                                <Form.Group controlId="formUsername">
+                                    <Form.Label id="username">
+                                        Username:
+                                    </Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter username"
+                                        value={username}
+                                        onChange={(e) =>
+                                            setUsername(e.target.value)
+                                        }
+                                    />
+                                    {usernameErr && <p>{usernameErr}</p>}
+                                </Form.Group>
 
-  return (
-    <Container className="loginContainer" fluid>
-      <Row className="justify-content-center login-view">
-        <Card id="login-card">
-          <Card.Body>
-            <Card.Title id="login-label">
-              {<h3>Welcome to BollyFlix!</h3>}
-            </Card.Title>
-            {showRegister === false ? (
-              <Form>
-                <Form.Group controlId="formUsername">
-                  <Form.Label id="username">Username:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                  {usernameErr && <p>{usernameErr}</p>}
-                </Form.Group>
+                                <Form.Group controlId="formPassword">
+                                    <Form.Label id="password">
+                                        Password
+                                    </Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Password"
+                                        value={password}
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
+                                    {passwordErr && <p>{passwordErr}</p>}
+                                </Form.Group>
 
-                <Form.Group controlId="formPassword">
-                  <Form.Label id="password">Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  {passwordErr && <p>{passwordErr}</p>}
-                </Form.Group>
+                                <Button
+                                    id="submit-btn"
+                                    variant="info"
+                                    type="submit"
+                                    onClick={handleSubmit}
+                                >
+                                    Login
+                                </Button>
 
-                <Button
-                  id="submit-btn"
-                  variant="info"
-                  type="submit"
-                  onClick={handleSubmit}
-                >
-                  Login
-                </Button>
-
-                <Button id="register-btn" variant="link" onClick={handleClick}>
-                  Register
-                </Button>
-              </Form>
-            ) : (
-              <RegistrationView toggleLogin={handleClick} />
-            )}
-          </Card.Body>
-        </Card>
-      </Row>
-    </Container>
-  );
+                                <Button
+                                    id="register-btn"
+                                    variant="link"
+                                    onClick={handleClick}
+                                >
+                                    Register
+                                </Button>
+                            </Form>
+                        ) : (
+                            <RegistrationView toggleLogin={handleClick} />
+                        )}
+                    </Card.Body>
+                </Card>
+            </Row>
+        </Container>
+    )
 }
 
 LoginView.propTypes = {
-  username: PropTypes.string,
-  password: PropTypes.string,
-};
+    username: PropTypes.string,
+    password: PropTypes.string,
+}
