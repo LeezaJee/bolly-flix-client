533b722adcaa2f1827ceab0af3135e3a180911e0
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-05 14:45:54 +0200

    added code for director-view

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,51 @@
+import { React } from 'react'
+import PropTypes from 'prop-types'
+import { MovieCard } from '../movie-card/movie-card'
+import { Col, Row, Card, Button } from 'react-bootstrap'
+
+export function GenreView(props) {
+    const { onBackClick, genre, movies } = props
+
+    return (
+        <>
+            <h1 className="genre-name">{genre.Name}</h1>
+            <Card>
+                <Card.Body>
+                    <h5 className="label">Description</h5>
+                    <p className="value">{genre.Description}</p>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <h5 className="director-movies">Movies</h5>
+                    <Row>
+                        {movies.map((m) => (
+                            <Col xs={12} md={6} lg={4} key={m._id}>
+                                <MovieCard movie={m} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Card.Body>
+            </Card>
+            <Button
+                className="mt-3"
+                variant="success"
+                type="button"
+                onClick={() => {
+                    onBackClick()
+                }}
+            >
+                Back
+            </Button>
+        </>
+    )
+}
+
+GenreView.propTypes = {
+    genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+    }).isRequired,
+    movies: PropTypes.array.isRequired,
+    onBackClick: PropTypes.func.isRequired,
+}
