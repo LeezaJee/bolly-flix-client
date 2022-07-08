a7e6e30a2388fecd210c2257e9a03393b77c1fa7
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-08 10:56:36 +0200

    added id to style button in update-user

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,68 +1,71 @@
 import React from 'react'
 import { Form, Button, Col, Row, Card } from 'react-bootstrap'
 
-export function UpdateUser(props) {
+export function UpdatedUser(props) {
     const user = props.userdata
-    const { handleSubmit, handleUpdate } = props
+    const { handleSubmit, handleUpdate, deleteProfile } = props
 
     return (
         <>
             <Col>
                 <Row>
-                    <Card className="bg-light" style={{ textAlign: 'left' }}>
-                        <Form
-                            className="profile-form text-dark"
-                            onSubmit={(e) => handleSubmit(e)}
+                    <Form
+                        className="profile-form"
+                        onSubmit={(e) => handleSubmit(e)}
+                    >
+                        <Form.Group controlId="formUsername" className="mb-3">
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="Username"
+                                defaultValue={user.Username}
+                                onChange={(e) => handleUpdate(e)}
+                                required
+                                placeholder="Enter new Username"
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formPassword" className="mb-3">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="Password"
+                                defaultValue=""
+                                onChange={(e) => handleUpdate(e)}
+                                required
+                                placeholder="Enter new Password"
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formEmail" className="mb-2">
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="Email"
+                                defaultValue={user.Email}
+                                onChange={(e) => handleUpdate(e)}
+                                required
+                                placeholder="Enter your Email address"
+                            />
+                        </Form.Group>
+
+                        <Button
+                            id="submit"
+                            variant="info"
+                            type="submit"
+                            onClick={handleSubmit}
                         >
-                            <Form.Group
-                                controlId="formUsername"
-                                className="mb-3"
-                            >
-                                <Form.Label>Username:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    defaultValue={user.Username}
-                                    onChange={(e) => handleUpdate(e)}
-                                    required
-                                    placeholder="Enter a Username"
-                                />
-                            </Form.Group>
-
-                            <Form.Group
-                                controlId="formPassword"
-                                className="mb-3"
-                            >
-                                <Form.Label>Password:</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    defaultValue=""
-                                    onChange={(e) => handleUpdate(e)}
-                                    required
-                                    placeholder="Your new password must be 8 or more characters"
-                                />
-                            </Form.Group>
-
-                            <Form.Group controlId="formEmail" className="mb-3">
-                                <Form.Label>Email:</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    defaultValue={user.Email}
-                                    onChange={(e) => handleUpdate(e)}
-                                    required
-                                    placeholder="Enter your email address"
-                                />
-                            </Form.Group>
-
-                            <Button
-                                variant="info"
-                                type="submit"
-                                onClick={handleSubmit}
-                            >
-                                Submit
-                            </Button>
-                            <h5>After updating, please log out and back in</h5>
-                        </Form>
-                    </Card>
+                            Submit
+                        </Button>
+                        <Button
+                            variant="danger"
+                            id="delete-btn"
+                            type="submit"
+                            onClick={deleteProfile}
+                        >
+                            Delete profile
+                        </Button>
+                    </Form>
                 </Row>
             </Col>
         </>
