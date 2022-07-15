9a58fead9589381b5aa354e87dc233acdf1ebfdd
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-15 21:27:34 +0200

    imported scss file into genre-view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -2,6 +2,7 @@ import { React } from 'react'
 import PropTypes from 'prop-types'
 import { MovieCard } from '../movie-card/movie-card'
 import { Col, Row, Card, Button } from 'react-bootstrap'
+import './genre-view.scss'
 
 export function GenreView(props) {
     const { onBackClick, genre, movies } = props
